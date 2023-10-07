403ceffc0bf5d695715fe274c7ff15eb18d00ba6
Hugo Cárdenas <hcgar00@gmail.com>
2023-10-07 14:21:42 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 gap-16">
+    <main className="flex h-screen flex-col items-center justify-between p-24 gap-16">
       <div className="flex flex-col items-center pt-32 h-screen gap-4">
         {/* <div>TITLE</div> */}
         <motion.div
@@ -16,7 +16,7 @@ export default function Home() {
             y: 0,
             rotate: 0,
           }}
-          transition={{ delay: 1, duration: 0.4 }}
+          transition={{ delay: 0, duration: 1 }}
         />
         <motion.div
           className="w-10 h-10 bg-blue-400"
@@ -26,7 +26,7 @@ export default function Home() {
             y: 0,
             rotate: 0,
           }}
-          transition={{ delay: 2, duration: 0.4 }}
+          transition={{ delay: 0, duration: 1 }}
         />
       </div>
     </main>
