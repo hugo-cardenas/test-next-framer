02d09491a3c6002347804b94b93da4e544935ed6
Hugo Cárdenas <hcgar00@gmail.com>
2023-10-07 14:36:39 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -13,30 +13,29 @@ export default function Home() {
   if (!enabled) return null;
 
   return (
-    <main className="flex h-screen flex-col items-center justify-between p-24 gap-16">
-      <div className="flex flex-col items-center pt-32 h-screen gap-4">
+    <main className="flex h-screen flex-col items-center justify-center p-24 gap-4">
+      <div className="flex flex-row justify-center items-center h-16 w-36 px-4 gap-4 bg-gray-200">
         {/* <div>TITLE</div> */}
         <motion.div
           className="w-10 h-10 bg-red-400"
-          initial={{ opacity: 0, y: 64, rotate: 360 }}
+          initial={{ opacity: 1, y: 80 }}
           animate={{
             opacity: 1,
             y: 0,
-            rotate: 0,
           }}
           transition={{ delay: 0, duration: 1 }}
         />
         <motion.div
           className="w-10 h-10 bg-blue-400"
-          initial={{ opacity: 0, y: 64, rotate: 360 }}
+          initial={{ opacity: 0, y: 80 }}
           animate={{
             opacity: 1,
             y: 0,
-            rotate: 0,
           }}
-          transition={{ delay: 0, duration: 1 }}
+          transition={{ delay: 1, duration: 1 }}
         />
       </div>
+      <div className="flex flex-row justify-center items-center h-16 w-36 px-4 gap-4 bg-gray-200" />
     </main>
   );
 }
